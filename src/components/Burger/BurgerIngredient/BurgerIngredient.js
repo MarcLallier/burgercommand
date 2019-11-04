d3e694eaea7d74e3e1510c1f5ba5614193b0be51
marc <lalliermarc8888@yahoo.fr>
2019-11-04 09:39:14 +0100

    burger hard coded so it is not dynamic yet, Burger.css created with media queries

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import propTypes from 'prop-types'
 
 import './BurgerIngredient.css'
@@ -10,33 +10,36 @@ class BurgerIngredient extends Component {
     switch (this.props.type) {
   
       case ('bread-bottom') :
-        ingredient = <div classname="BreadBottom"></div>
+        ingredient = <div className="BreadBottom"></div>
         break
       
       case ('bread-top') :
         ingredient = (
-          <div classname="BreadTop">
-            <div classname="Seeds1"></div>
-            <div classname="Seeds2"></div>
+          <div className="BreadTop">
+            <div className="Seeds1"></div>
+            <div className="Seeds2"></div>
           </div>
         )
         break
       
       case ('meat') : 
-        ingredient = <div classname="BreadBottom"></div>
+        ingredient = <div className="Meat"></div>
         break
   
       case ('cheese') : 
-        ingredient = <div classname="Cheese"></div>
+        ingredient = <div className="Cheese"></div>
         break
   
       case ('salad') : 
-        ingredient = <div classname="Salad"></div>
+        ingredient = <div className="Salad"></div>
         break
   
       case ('beacon') : 
-        ingredient = <div classname="Beacon"></div>
+        ingredient = <div className="Beacon"></div>
         break
+      
+      default : 
+          ingredient = null
   
     }
     return ingredient
