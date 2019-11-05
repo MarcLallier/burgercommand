34270c0631d191af313a027994f6e97aee5de548
marc <lalliermarc8888@yahoo.fr>
2019-11-05 16:22:03 +0100

    fix name of state beacon in bacon in BurgerBuildre, adding removeIngredientHandler with logic, ans a disable function when ingredients state ==0

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -16,6 +16,8 @@ const buildControls = (props) => (
     {controls.map(ctrl => (
       <BuildControl 
         added={() => props.ingredientAdded(ctrl.type)}
+        disabled= {props.disabled[ctrl.type]} 
+        removed={() => props.ingredientRemoved(ctrl.type)}
         key={ctrl.label} 
         label={ctrl.label} 
       />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,7 +7,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 const INGREDIENT_PRICES = {
   salad : 0.5,
-  beacon : 0.4,
+  bacon : 0.4,
   cheese : 0.7,
   meat : 1.3,
 }
@@ -19,7 +19,7 @@ class BurgerBuilder extends Component {
   state = {
     ingredients : {
       salad : 0,
-      beacon : 0,
+      bacon : 0,
       cheese : 0,
       meat : 0,
     },
@@ -41,11 +41,32 @@ class BurgerBuilder extends Component {
   }
 
   removeIngredientHandler = (type) =>{
-
+    const oldCount = this.state.ingredients[type]
+    if (oldCount <= 0){
+      return
+    }
+    const updatedCount = oldCount - 1
+    const updatedIngredients = {
+      ...this.state.ingredients
+    }
+    updatedIngredients[type] = updatedCount 
+    const priceDeduction = INGREDIENT_PRICES[type]
+    const oldPrice = this.state.totalPrice
+    const newPrice = oldPrice - priceDeduction
+    this.setState({totalPrice : newPrice, ingredients : updatedIngredients})
   }
 
   render() {
-    return (
+
+    const disabledInfo = {
+      ...this.state.ingredients
+    }
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0
+    }
+
+
+    return ( 
       
       <Aux>
         <div>
@@ -54,6 +75,8 @@ class BurgerBuilder extends Component {
         <div>
           <BuildControls 
             ingredientAdded = {this.addIngredientHandler}
+            ingredientRemoved = {this.removeIngredientHandler}
+            disabled = {disabledInfo}
           />
         </div> 
       </Aux>
