7c0b9830898a1d9e9685efcad1585ffffef75d9d
marc <lalliermarc8888@yahoo.fr>
2019-11-07 16:14:06 +0100

    SideDrawer component added,and css. style height added for logo for mobile device size.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,13 @@ import React from 'react'
 import Aux from '../../hoc/Aux'
 import './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = (props) => (
 
   <Aux>
     <Toolbar/>
+    <SideDrawer />
     <div>SideDrawer, Backdrop</div>
     <main className="Content">
       {props.children}
