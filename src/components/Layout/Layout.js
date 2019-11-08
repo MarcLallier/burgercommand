86743cde620512ef6f62b45e7af1d467b2fe29bd
marc <lalliermarc8888@yahoo.fr>
2019-11-08 16:02:32 +0100

    put the backdrop with the sidedrawer to make it disappear.Turned the layout component in class, to have a state false/true to show sidedrawer.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import Aux from '../../hoc/Aux'
 import './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-
-  <Aux>
-    <Toolbar/>
-    <SideDrawer />
-    <div>SideDrawer, Backdrop</div>
-    <main className="Content">
-      {props.children}
-    </main>
-  </Aux>
-)
+class Layout extends Component {
+  state = {
+    showSideDrawer : true
+  }
 
 
-export default layout
+
+  SideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer : false})
+  }
+
+
+
+
+  render () {
+    return (
+      <Aux>
+      <Toolbar/>
+      <SideDrawer 
+        open={this.state.showSideDrawer} 
+        closed={this.SideDrawerClosedHandler}
+      />
+      <div>SideDrawer, Backdrop</div>
+      <main className="Content">
+        {this.props.children}
+      </main>
+    </Aux>
+    )
+  }
+}
+
+
+
+
+
+export default Layout
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,8 +7,10 @@ import NavigationItems from '../NavigationItems/NavigationItems'
 const toolbar = (props) => (
   <header className="Toolbar">
     <div>MENU</div>
-    <Logo height="80%"/>
-    <nav>
+    <div className="toolbarLogo">
+      <Logo height="80%"/>
+    </div>
+    <nav className="DesktopOnly">
       <NavigationItems />
     </nav>
   </header>
